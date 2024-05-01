3c1e2c31889a428cdad4de02da00583374a33463
Mrichko Bohdan <zaq4769@gmail.com>
2024-05-01 15:02:58 +0300

    delete middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,8 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import { authReducer } from './AuthSlice/slice';
-
-const middleware = getDefaultMiddleware({
-    serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-});
 
 const authPersistConfig = {
     key: 'auth',
@@ -20,7 +14,7 @@ export const store = configureStore({
     reducer: {
         auth: persistReducer(authPersistConfig, authReducer),
     },
-    middleware,
+
     devTools: process.env.NODE_ENV === 'development',
 });
 
