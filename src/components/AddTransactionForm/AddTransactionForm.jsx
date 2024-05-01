ad88da8cecaef7000379a60576bb53929d915e55
Vladyslav Salenik <vladyslav.salenik@gmail.com>
2024-05-01 23:32:50 +0300

    add wrap

--- src/components/AddTransactionForm/AddTransactionForm.jsx
+++ src/components/AddTransactionForm/AddTransactionForm.jsx
@@ -1,9 +1,12 @@
 import s from './AddTransactionForm.module.css';
 import { useState } from 'react';
 import clsx from 'clsx';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function AddTransactionForm() {
     const [isChecked, setIsChecked] = useState(false);
+    const [startDate, setStartDate] = useState(new Date());
     const handleChange = () => {
         setIsChecked(!isChecked);
     };
@@ -57,6 +60,29 @@ function AddTransactionForm() {
                     )}
                 </label>
                 {isChecked ? <span className={clsx(s.span_text, s.expense_active)}>Expense</span> : <span className={s.span_text}>Expense</span>}
+            </div>
+            <div className={s.sum_data_wrap}>
+                <div className={s.sum_wrap}>
+                    <input type="text" />
+                </div>
+                <div className={s.data_wrap}>
+                    <DatePicker selected={startDate} onChange={date => setStartDate(date)} dateFormat="dd.MM.yyyy" />
+                    <div className={s.svg_wrap}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <g clip-path="url(#clip0_60_133)">
+                                <path
+                                    d="M9 11H7V13H9V11ZM13 11H11V13H13V11ZM17 11H15V13H17V11ZM19 4H18V2H16V4H8V2H6V4H5C3.89 4 3.01 4.9 3.01 6L3 20C3 21.1 3.89 22 5 22H19C20.1 22 21 21.1 21 20V6C21 4.9 20.1 4 19 4ZM19 20H5V9H19V20Z"
+                                    fill="#734AEF"
+                                />
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_60_133">
+                                    <rect width="24" height="24" fill="white" />
+                                </clipPath>
+                            </defs>
+                        </svg>
+                    </div>
+                </div>
             </div>
         </div>
     );
