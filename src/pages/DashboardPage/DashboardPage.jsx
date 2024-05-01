ec58cbf9feda26c0c4276a3a5fe994c423e205be
Vladyslav Salenik <vladyslav.salenik@gmail.com>
2024-05-01 12:43:49 +0300

    Update DashboardPage.jsx

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,12 +1,6 @@
-<<<<<<< Updated upstream
-import { Outlet } from "react-router-dom";
-import Header from "../../components/Header/Header";
-import Navigation from '../../components/Navigation/Navigation';
-=======
 import { Outlet } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import ModalAddTransaction from '../../components/ModalAddTransaction/ModalAddTransaction';
->>>>>>> Stashed changes
 
 function DashboardPage() {
     return (
