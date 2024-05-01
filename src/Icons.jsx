1c7ec2caa9acb04192583c155f7b114ba8bc3bf5
IrinaHarkusha <irenagarkushalw@gmail.com>
2024-05-01 12:05:22 +0300

    commit css

---
+++ src/Icons.jsx
@@ -0,0 +1,10 @@
+// import React from "react";
+import Icons from "./sprite.svg"
+
+export const Icon = ({ id, className }) => {
+    return (
+        <svg className={className}>
+            <use  href={Icons + id}></use>
+        </svg>
+    )
+}
