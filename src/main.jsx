fc1d5d7e4bb68f5fcadcafcc5e891c5cec0e1836
Mrichko Bohdan <zaq4769@gmail.com>
2024-05-01 16:26:25 +0300

    remove coment from Provider in main

--- src/main.jsx
+++ src/main.jsx
@@ -13,12 +13,12 @@ import { persistor, store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        {/* <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}> */}
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-        {/* </PersistGate>
-        </Provider> */}
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </PersistGate>
+        </Provider>
     </React.StrictMode>
 );
