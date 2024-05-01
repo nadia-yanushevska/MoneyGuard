a331f02a555347fa363f37c29477f2e412ad4db7
Rudakivska-Marina <rudakivska.marina@gmail.com>
2024-05-01 12:05:09 +0300

    create Navigation

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,36 @@
+import { TiHome } from 'react-icons/ti';
+import { FaDollarSign } from 'react-icons/fa';
+import { BiStats } from 'react-icons/bi';
+
+export const Navigation = () => {
+  return (
+    <StyledNav className="navigation">
+      <ul className="list">
+        <li>
+          <StyledLinkHome className="linkHome" to="/home">
+            <div className="boxIcon">
+              <TiHome className="icon" />
+            </div>
+            <StyledSpan className="styledSpan">Home</StyledSpan>
+          </StyledLinkHome>
+        </li>
+        <li>
+          <StyledLinkHome className="linkHome" to="/statistics">
+            <div className="boxIcon">
+              <BiStats className="icon iconRevers" />
+            </div>
+
+            <StyledSpan className="styledSpan">Statistics</StyledSpan>
+          </StyledLinkHome>
+        </li>
+        <li>
+          <StyledLinkHome className="linkHome" to="/currency">
+            <div className="boxIconDollar">
+              <FaDollarSign className="icon dollar" />
+            </div>
+          </StyledLinkHome>
+        </li>
+      </ul>
+    </StyledNav>
+  );
+};
