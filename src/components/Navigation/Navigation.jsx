00c5897fc36f3070d1993592acdde3ea3fc035b3
Rudakivska-Marina <rudakivska.marina@gmail.com>
2024-05-02 19:28:57 +0300

    create

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -6,18 +6,17 @@ const Navigation = () => {
     const getClasses = isActive => (isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink}`);
     return (
         <nav className={styles.navigation}>
-            <NavLink to="/dashboard" className={({ isActive }) => getClasses(isActive)}>
-                <Icon id="#icon-home" className={`${styles.dollarIcon} ${styles.linkIcon}`} />
+            <NavLink to="header" className={({ isActive }) => getClasses(isActive)}>
+                <Icon id="#icon-home" className={`${styles.homeIcon} ${styles.linkIcon}`} />
                 <span className={styles.linkText}>Home</span>
             </NavLink>
 
             <NavLink to="statistics" className={({ isActive }) => getClasses(isActive)}>
-                <Icon id="#icon-graphic" className={`${styles.dollarIcon} ${styles.linkIcon}`} />
+                <Icon id="#icon-graphic" className={`${styles.graphicIcon} ${styles.linkIcon}`} />
                 <span className={styles.linkText}>Statistics</span>
             </NavLink>
-
-            {/* Має бути видно тільки на мобільній версії */}
-            <NavLink to="currency" className={({ isActive }) => getClasses(isActive)}>
+            
+            <NavLink to="currency" className={({ isActive }) => isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink} ${styles.currencyLink}` }>
                 <Icon id="#icon-dollar" className={`${styles.dollarIcon} ${styles.linkIcon}`} />
                 <span className={styles.linkText}>Currency</span>
             </NavLink>
