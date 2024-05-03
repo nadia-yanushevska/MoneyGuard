0b6233037cefe69e2d8b055fcffb648e94d01327
Mrichko Bohdan <zaq4769@gmail.com>
2024-05-03 10:30:02 +0300

    add styles for form

--- src/components/CustomField/CustomField.jsx
+++ src/components/CustomField/CustomField.jsx
@@ -1,14 +1,20 @@
 import { ErrorMessage, Field } from 'formik';
 import { useState } from 'react';
 
+import style from './customField.module.css';
+// import { Icon } from '../../Icons';
+
 const CustomField = ({ type, name, placeholder }) => {
     const [showPassword, setShowPassword] = useState(false);
     return (
-        <label>
-            {name}
+        <label className={style.label}>
             <div>
-                <Field type={showPassword ? 'text' : type} name={name} placeholder={placeholder} />
-                {type === 'password' && <button type="button" onClick={() => setShowPassword(!showPassword)}></button>}
+                <Field className={style.input} type={showPassword ? 'text' : type} name={name} placeholder={placeholder} />
+                {type === 'password' && (
+                    <button className={style.buttonPassword} type="button" onClick={() => setShowPassword(!showPassword)}>
+                        {/* <Icon id={showPassword ? 'eye-slash' : 'eye'} className={style.icon} size="18" /> */}
+                    </button>
+                )}
             </div>
             <ErrorMessage name={name} component={'p'} />
         </label>
---
+++ src/schemas/schemas.js
@@ -0,0 +1,16 @@
+import * as yup from 'yup';
+
+export const registerSchema = yup.object().shape({
+    username: yup.string().min(3, 'Name must be at least 3 characters').required('Name is required'),
+    email: yup.string().email('Invalid email').required('Email is required'),
+    password: yup.string().min(7, 'Password must be at least 7 characters').required('Password is required'),
+    confirmPassword: yup
+        .string()
+        .oneOf([yup.ref('password'), null], 'Passwords must match')
+        .required('Confirm Password is required'),
+});
+
+export const loginSchema = yup.object().shape({
+    email: yup.string().email('Invalid email').required('Email is required'),
+    password: yup.string().min(7, 'Password must be at least 7 characters').required('Password is required'),
+});
