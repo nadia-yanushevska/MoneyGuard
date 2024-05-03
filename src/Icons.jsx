b7b799c62dfc1f2974c8d733c1069e34c7c22e22
Mrichko Bohdan <zaq4769@gmail.com>
2024-05-03 10:30:02 +0300

    add styles for form

--- src/Icons.jsx
+++ src/Icons.jsx
@@ -1,11 +1,10 @@
-import Icons from "./sprite.svg"
+// import React from "react";
+import Icons from './sprite.svg';
 
 export const Icon = ({ id, className }) => {
     return (
         <svg className={className}>
             <use href={Icons + id}></use>
         </svg>
-    )
-}
-
-
+    );
+};
