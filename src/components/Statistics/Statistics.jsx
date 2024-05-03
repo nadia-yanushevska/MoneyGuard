1f0f83f0b780b04916dabad62f51f9e55e5b066c
SobDim <101928077+SobDim@users.noreply.github.com>
2024-05-03 21:36:10 +0300

    "Add chart.js, react-chartjs-2 for visualizing statistics; fetch transactions summary by period on component mount"
    This commit adds the necessary dependencies and components to display a doughnut chart showing the summary of expenses by category.

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,16 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ChartDoughnut from '../ChartDoughnut/ChartDoughnut';
 import s from './Statistics.module.css';
-// import StatisticsTable from './StatisticsTable';
 import { selectSummary } from '../../redux/Statistics/selectors';
 import coloredCategoriesMap from './coloredCategoriesMap';
 import StatisticsTable from './StatisticsTable';
+import { useEffect } from 'react';
+import { getTransactionsSummaryByPeriod } from '../../redux/Statistics/operations';
+import { getCurrentMonthYear } from '../../helpers/dateHelper';
 
 function Statistics() {
   const transactions = useSelector(selectSummary);
-  console.log(transactions);
+  console.log(transactions.categoriesSummary);
   const expense = transactions.categoriesSummary
-    ? transactions.filter(transaction => transaction.type === 'EXPENSE')
+    ? transactions.categoriesSummary.filter(transaction => transaction.type === 'EXPENSE')
     : [];
 
   const expenseTotal = transactions.expenseSummary;
@@ -20,7 +22,10 @@ function Statistics() {
     ...item,
     color: coloredCategoriesMap.get(item.name),
   }));
-
+  const dispatch = useDispatch();
+  useEffect(() => {
+      dispatch(getTransactionsSummaryByPeriod(getCurrentMonthYear()));
+  }, [dispatch]);
   //TODO: Change div-s to components, remove temp classes
   return (
     <div className={s.container}>
