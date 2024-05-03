42b8439da6a6893988e3e1f7f9483b1c2e9a8522
kostak94 <slavakostak2010@gmail.com>
2024-05-03 12:22:15 +0200

    added balne component to dashboard and app

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -4,6 +4,7 @@ import Navigation from '../../components/Navigation/Navigation';
 import s from './DashboardPage.module.css';
 import Currency from '../../components/Currency/Currency';
 import useMedia from '../../hooks/useMedia';
+import Balance from '../../components/Balance/Balance';
 
 function DashboardPage() {
     const { isMobile } = useMedia();
@@ -15,7 +16,7 @@ function DashboardPage() {
             <div className={s.container}>
                 <div className={s.column_narrow}>
                     <Navigation />
-                    <div>Balance</div>
+                    <Balance />
                     <Currency />
                 </div>
                 <div className={`${s.column} ${s.temp_1}`}>
