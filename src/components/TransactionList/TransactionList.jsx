bcb97c53ebcfd866c060d6f547ee4fbbfa2ad353
Nadia <nadiazlagoda@gmail.com>
2024-05-05 20:57:44 +0300

    loader/error

--- src/components/TransactionList/TransactionList.jsx
+++ src/components/TransactionList/TransactionList.jsx
@@ -1,17 +1,22 @@
 import { useSelector } from 'react-redux';
 import s from './TransactionList.module.css';
-import { selectTransactions } from '../../redux/Transactions/selectors';
+import { selectTransError, selectTransLoading, selectTransactions } from '../../redux/Transactions/selectors';
 import TransactionItem from '../TransactionItem/TransactionItem';
 import { selectCategories } from '../../redux/Statistics/selectors';
 import { getFormattedTransactions } from '../../helpers/transactionsFormatter';
+import Loader from '../Loader/Loader';
 
 function TransactionList() {
     const reduxTransactions = useSelector(selectTransactions);
+    const isLoading = useSelector(selectTransLoading);
+    const isError = useSelector(selectTransError);
     const categories = useSelector(selectCategories);
 
     return (
         <>
-            {reduxTransactions.length === 0 ? (
+            {isLoading && <Loader />}
+            {isError && <p>Oops, something went wrong...</p>}
+            {!isLoading && reduxTransactions.length === 0 ? (
                 <p>No transactions available.</p>
             ) : (
                 <ul className={s.list}>
