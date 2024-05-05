6694a006503f8e0c544c6b5f8815dcfe016c689d
Nadia <nadiazlagoda@gmail.com>
2024-05-05 12:54:43 +0300

    transactions-scroll

--- src/components/TransactionList/TransactionList.jsx
+++ src/components/TransactionList/TransactionList.jsx
@@ -8,17 +8,14 @@ import { getFormattedTransactions } from '../../helpers/transactionsFormatter';
 function TransactionList() {
     const reduxTransactions = useSelector(selectTransactions);
     const categories = useSelector(selectCategories);
-    console.log(reduxTransactions);
 
-    const transactions = getFormattedTransactions(reduxTransactions, categories);
     return (
         <>
-            {transactions.length === 0 ? (
+            {reduxTransactions.length === 0 ? (
                 <p>No transactions available.</p>
             ) : (
                 <ul className={s.list}>
-                    {transactions.map(({ id, ...item }, idx) => {
-                        // console.log(id);
+                    {getFormattedTransactions(reduxTransactions, categories).map(({ id, ...item }, idx) => {
                         return <TransactionItem key={id} id={id} transaction={item} first={idx === 0} />;
                     })}
                 </ul>
