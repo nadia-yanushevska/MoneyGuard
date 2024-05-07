3196b7e854e076e462059c8fee9a775a02eaee10
Nadia <nadiazlagoda@gmail.com>
2024-05-07 12:38:37 +0300

    statitics-fix

--- src/components/Statistics/StatisticsTable.jsx
+++ src/components/Statistics/StatisticsTable.jsx
@@ -1,45 +1,41 @@
 import s from './StatisticsTable.module.css';
 
 const StatisticsTable = ({ data, expenseTotal, incomeTotal }) => {
-   
-  return (
-    <div>
-      <div className={s.table_head}>
-        <p>Category</p>
-        <p>Sum</p>
-      </div>
-      <div className={s.list_wrapper}>
-        <ul>
-          {data.length
-            ? data.map((category, index) => (
-                <li key={index}>
-                  <div className={s.item_wrapper}>
-                    <p className={s.first_column}>
-                      <span
-                        className={s.span_indicator}
-                        style={{ backgroundColor: `${category.color}` }}
-                      ></span>
-                      {category.name}
-                    </p>
+    return (
+        <div>
+            <div className={s.table_head}>
+                <p>Category</p>
+                <p>Sum</p>
+            </div>
+            <div className={s.list_wrapper}>
+                <ul>
+                    {data.length
+                        ? data.map((category, index) => (
+                              <li key={index}>
+                                  <div className={s.item_wrapper}>
+                                      <p className={s.first_column}>
+                                          <span className={s.span_indicator} style={{ backgroundColor: `${category.color}` }}></span>
+                                          {category.name}
+                                      </p>
 
-                    <p>{Math.abs(category.total).toFixed(2)}</p>
-                  </div>
-                </li>
-              ))
-            : [] && <p>No data</p>}
-        </ul>
+                                      <p>{Math.abs(category.total).toFixed(2)}</p>
+                                  </div>
+                              </li>
+                          ))
+                        : ''}
+                </ul>
 
-        <div className={s.table_bottom}>
-          <p>Expenses:</p>
-          <span className={s.expense}>{Math.abs(expenseTotal).toFixed(2)}</span>
+                <div className={s.table_bottom}>
+                    <p>Expenses:</p>
+                    <span className={s.expense}>{Math.abs(expenseTotal).toFixed(2)}</span>
+                </div>
+                <div className={s.table_bottom}>
+                    <p>Income:</p>
+                    <span className={s.income}>{Math.abs(incomeTotal).toFixed(2)}</span>
+                </div>
+            </div>
         </div>
-        <div className={s.table_bottom}>
-          <p>Income:</p>
-          <span className={s.income}>{Math.abs(incomeTotal).toFixed(2)}</span>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default StatisticsTable;
